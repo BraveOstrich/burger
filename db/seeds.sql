f00f71caa1bf73519f8041cd40539042db2a474f
Steve White <stevewhitecodes@gmail.com>
2019-04-12 17:30:31 -0500

    Completed App

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,8 @@
 INSERT INTO burgers (id, name, devoured)
 VALUES (1, "Jalapeno Cheeseburger", false),
 (2, "El Diablo Cheeseburger", false),
-(3, "Impossible Burger", false);
+(3, "Impossible Burger", false),
+(4, "Whopper", false),
+(5, "Big Mac", false),
+(6, "Baconator", false),
+(7, "Son of Baconator", false);
